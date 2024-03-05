6b861de5e086b13eeb58d45c9b7715ceed3d60a2
Nicolas Delplanque <nicolas@delplanque.eu>
2024-03-05 17:15:43 +0100

    feat(PhotoService): removed service

---
+++ src/Controller/AppUserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\AppUser;
+use App\Form\AppUserType;
+use App\Repository\AppUserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/users')]
+class AppUserController extends AbstractController
+{
+    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    public function index(AppUserRepository $appUserRepository): JsonResponse
+    {
+        return $this->json([
+            'users' => $appUserRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/', name: 'app_user_new', methods: ['POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $appUser = new AppUser();
+        $form = $this->createForm(AppUserType::class, $appUser);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($appUser);
+            $entityManager->flush();
+
+            return $this->json([
+                'app_user' => $appUser,
+                'form' => $form,
+            ]);
+        }
+
+        return $this->json([
+            'app_user' => $appUser,
+            'form' => $form,
+        ], 400);
+    }
+
+    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+    public function show(AppUser $appUser): Response
+    {
+        return $this->render('app_user/show.html.twig', [
+            'app_user' => $appUser,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['PUT'])]
+    public function edit(Request $request, AppUser $appUser, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(AppUserType::class, $appUser);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('app_user/edit.html.twig', [
+            'app_user' => $appUser,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user_delete', methods: ['DELETE'])]
+    public function delete(Request $request, AppUser $appUser, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $appUser->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($appUser);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/AppUserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Album;
+use App\Entity\AppUser;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AppUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('password')
+            ->add('is_admin')
+            ->add('shared_albums', EntityType::class, [
+                'class' => Album::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AppUser::class,
+        ]);
+    }
+}
