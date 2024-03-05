f0637c5bdfb7cb29ec7b27dd590fbabd0c7426b1
Nicolas Delplanque <nicolas@delplanque.eu>
2024-03-05 11:45:44 +0100

    feat(HealthCheckController): fixed controller

--- src/Controller/HealthCheckController.php
+++ src/Controller/HealthCheckController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HealthCheckController extends AbstractController
@@ -12,6 +11,6 @@ class HealthCheckController extends AbstractController
     #[Route('/health-check', name: 'app_health_check')]
     public function index(): JsonResponse
     {
-        return $this->json(['success'=>true]);
+        return $this->json(['success' => true]);
     }
 }
