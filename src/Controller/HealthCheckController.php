1fb360057837d07accaabc32a82bf1c91417f0c4
Nicolas Delplanque <nicolas@delplanque.eu>
2024-03-05 10:44:25 +0100

    feat(Skeleton): edited skeleton

---
+++ src/Controller/HealthCheckController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HealthCheckController extends AbstractController
+{
+    #[Route('/health-check', name: 'app_health_check')]
+    public function index(): JsonResponse
+    {
+        return $this->json(['success'=>true]);
+    }
+}
