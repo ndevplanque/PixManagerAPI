67955c710722132e63f5a5a2291af248a9961709
Nicolas Delplanque <nicolas@delplanque.eu>
2024-03-11 15:42:26 +0100

    chore(skeleton): added exception subscriber

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        $event->setResponse(new JsonResponse([
+            'status' => $exception instanceof HttpException ? $exception->getStatusCode() : 500,
+            'message' => $exception->getMessage()
+        ]));
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
