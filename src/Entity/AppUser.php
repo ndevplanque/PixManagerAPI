ccf7530417cccb532383cd47ca60a4b82acc5477
Nicolas Delplanque <nicolas@delplanque.eu>
2024-03-11 15:41:33 +0100

    chore(skeleton): added roles to user

--- src/Entity/AppUser.php
+++ src/Entity/AppUser.php
@@ -6,15 +6,20 @@ use App\Repository\AppUserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: AppUserRepository::class)]
-class AppUser
+class AppUser implements UserInterface, PasswordAuthenticatedUserInterface
 {
+    #[Groups('users')]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Groups('users')]
     #[ORM\Column(length: 255)]
     private ?string $email = null;
 
@@ -22,18 +27,24 @@ class AppUser
     private ?string $password = null;
 
     #[ORM\Column]
+    private array $roles = [];
+
+    #[Groups('users')]
+    #[ORM\Column]
     private ?bool $is_admin = null;
 
-    #[ORM\ManyToMany(targetEntity: Album::class, inversedBy: 'shared_to')]
-    private Collection $shared_albums;
-
+    #[Groups('users')]
     #[ORM\OneToMany(targetEntity: Album::class, mappedBy: 'owner', orphanRemoval: true)]
     private Collection $owned_albums;
 
+    #[Groups('users')]
+    #[ORM\ManyToMany(targetEntity: Album::class, inversedBy: 'shared_to')]
+    private Collection $shared_albums;
+
     public function __construct()
     {
-        $this->shared_albums = new ArrayCollection();
         $this->owned_albums = new ArrayCollection();
+        $this->shared_albums = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -53,7 +64,44 @@ class AppUser
         return $this;
     }
 
-    public function getPassword(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string)$this->email;
+    }
+
+    /**
+     * @return list<string>
+     * @see UserInterface
+     *
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
         return $this->password;
     }
@@ -65,7 +113,16 @@ class AppUser
         return $this;
     }
 
-    public function isIsAdmin(): ?bool
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function isAdmin(): ?bool
     {
         return $this->is_admin;
     }
@@ -73,30 +130,6 @@ class AppUser
     public function setIsAdmin(bool $is_admin): static
     {
         $this->is_admin = $is_admin;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Album>
-     */
-    public function getSharedAlbums(): Collection
-    {
-        return $this->shared_albums;
-    }
-
-    public function addSharedAlbum(Album $sharedAlbum): static
-    {
-        if (!$this->shared_albums->contains($sharedAlbum)) {
-            $this->shared_albums->add($sharedAlbum);
-        }
-
-        return $this;
-    }
-
-    public function removeSharedAlbum(Album $sharedAlbum): static
-    {
-        $this->shared_albums->removeElement($sharedAlbum);
 
         return $this;
     }
@@ -131,7 +164,33 @@ class AppUser
         return $this;
     }
 
-    public function getUsername(): string{
+
+    /**
+     * @return Collection<int, Album>
+     */
+    public function getSharedAlbums(): Collection
+    {
+        return $this->shared_albums;
+    }
+
+    public function addSharedAlbum(Album $sharedAlbum): static
+    {
+        if (!$this->shared_albums->contains($sharedAlbum)) {
+            $this->shared_albums->add($sharedAlbum);
+        }
+
+        return $this;
+    }
+
+    public function removeSharedAlbum(Album $sharedAlbum): static
+    {
+        $this->shared_albums->removeElement($sharedAlbum);
+
+        return $this;
+    }
+
+    public function getUsername(): string
+    {
         return explode('@', $this->email)[0];
     }
 
