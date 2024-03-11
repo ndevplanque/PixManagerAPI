ceacdabbea37a4a89ffbed1a8e6bec2b6b18fdc7
Nicolas Delplanque <nicolas@delplanque.eu>
2024-03-11 16:04:32 +0100

    chore(skeleton): added better exceptions

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -4,6 +4,7 @@ namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -14,9 +15,15 @@ class ExceptionSubscriber implements EventSubscriberInterface
     {
         $exception = $event->getThrowable();
 
+        $status = $exception instanceof HttpException
+            ? $exception->getStatusCode()
+            : 500;
+
         $event->setResponse(new JsonResponse([
-            'status' => $exception instanceof HttpException ? $exception->getStatusCode() : 500,
-            'message' => $exception->getMessage()
+            'success' => false,
+            'status' => $status,
+            'message' => Response::$statusTexts[$status],
+            'reason' => $exception->getMessage(),
         ]));
     }
 
