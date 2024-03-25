36d0d17b92a121f27203d2c3ccd2bdb504cde955
Nicolas Delplanque <nicolas@delplanque.eu>
2024-03-25 23:53:50 +0100

    feat(PhotoListingByUserService): listing can be done according to accuracy score

--- src/Service/Photo/PhotoListingByUserService.php
+++ src/Service/Photo/PhotoListingByUserService.php
@@ -11,13 +11,37 @@ use App\Response\PhotoResponse;
 
 class PhotoListingByUserService
 {
-    public function handle(AppUser $user): PhotoListingByUserResponse
+    public function handle(AppUser $user, ?string $search = null): PhotoListingByUserResponse
     {
-        return new PhotoListingByUserResponse(
-            array_map(
-                fn(Photo $photo) => new PhotoResponse($photo),
-                $user->getPhotos()->getValues()
-            )
-        );
+        if ($search === null) {
+            $photos = [];
+
+            foreach ($user->getPhotos()->getIterator() as $photo) {
+                $photos[] = new PhotoResponse($photo);
+            }
+
+            return new PhotoListingByUserResponse($photos);
+        }
+
+        $photosById = [];
+        $accuracyById = [];
+
+        foreach ($user->getPhotos()->getIterator() as $photo) {
+            $photosById[$photo->getId()] = $photo;
+            $accuracyById[$photo->getId()] = $photo->getAccuracyScore($search);
+        }
+
+        // Sort array by ascending values (because accuracyScore = the least, the most accurate)
+        asort($accuracyById);
+
+        $idsByAccury = array_keys($accuracyById);
+
+        $photosByAccuracy = [];
+
+        foreach ($idsByAccury as $id) {
+            $photosByAccuracy[] = new PhotoResponse($photosById[$id]);
+        }
+
+        return new PhotoListingByUserResponse($photosByAccuracy);
     }
 }
